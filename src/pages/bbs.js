5cc5f523268383dcc4bbfd8950fa1165c4b4b8a8
yamat0jp <yamat0jp@yahoo.co.jp>
2024-03-10 18:56:04 +0900

    jsonのポストに問題あり

--- src/pages/bbs.js
+++ src/pages/bbs.js
@@ -1,14 +1,26 @@
-import React,{useState} from 'react'
+import React,{useEffect, useState} from 'react'
 import axios from 'axios'
 import {Link} from 'gatsby'
 
 function TitlePage() {
     const [value,setValue] = useState([]);
-    async function Mount() {
-        const {data} = await axios.get("http://localhost:8080/apis/articles");                      
-        setValue(data.map(user => user.name));
-    };  
-    Mount();
+    useEffect(() => {
+        async function Mount() {
+            const {data} = await axios.get("http://localhost:8080/apis/articles");    
+            console.log(data);                  
+            setValue(data.titles.map(user => {
+                return (
+                    <>
+                    <Link to="/threads">({user.titlenum}){user.title}</Link>
+                        ::(date){user.datetime}<br />
+                    {user.name}
+                    <hr />
+                    </>
+                )
+            }));        
+        };
+        Mount();
+    },[]);      
     return (
         <>            
             {value}
--- src/pages/threads.js
+++ src/pages/threads.js
@@ -1,13 +1,14 @@
-import React,{useState} from 'react';
+import React,{useEffect, useState} from 'react';
 import {Input,Form,Button} from 'antd';
 import axios from 'axios'
 
 const {TextArea} = Input;
 function Register(props) {
     async function onFinish(values) { 
-        const json = {"title":props.title,"name":values.name,"comment":values.comment+values.code};
-        console.log(values);    
-        await axios.post("http://localhost:8080/apis/register",values);
+        const json = {"titlenum":props.titlenum,"title":props.title,
+            "name":values.name,"comment":values.comment,"code":values.code};        
+        console.log(json);
+        await axios.post("http://localhost:8080/apis/register/"+props.titlenum,json);
     };
     return (
         <>
@@ -31,29 +32,29 @@ function Register(props) {
 function TopPage(props) {    
     const [value,setValue] = useState([]);
     const [title,setTitle] = useState("");
-    async function Mount() {
-        const json = await axios.get("http://localhost:8080/apis/articles/2");                   
-        const comments = json.comments;
-        setTitle(json.title);
-        console.log(comments);    
-        const list = [];
-        for (var i=0;i<comments.length;i++) list.push([i,comments[i]]);
-        const field = list.map(res => {
-            return (
-                <>
-                お名前({res.name})：：日付{res.date}
-                {res.comment}
-                {res.code}
-                <hr />
-                </>
-            )
-        });
-        setValue(field);
-    };
-    Mount();
+    const num = 2;
+    useEffect(()=>{
+        async function Mount() {
+            const json = await axios.get("http://localhost:8080/apis/articles/"+num);                                           
+            console.log(json.data.title);
+            setTitle(json.data.title);    
+            const field = json.data.comments.map(res => {
+                return (
+                    <>
+                    お名前({res.name})：：日付{res.date}
+                    {res.comment}
+                    {res.code}
+                    <hr />
+                    </>
+                )
+            });
+            setValue(field);
+        };
+        Mount();
+    },[]);
     return (
         <>
-            <Register />
+            <Register title={title} titlenum={num} />
             {title}
             <p>{value}</p>
             <a href='/'>もどる</a>
